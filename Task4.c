7aa22067def54eb881b87c4826c3e0c6362309df
aditbhatia07 <83564971+aditbhatia07@users.noreply.github.com>
2025-12-06 00:06:07 +0530

    Create Task4.c

---
+++ Task4.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <string.h>
+//Create a program to simulate a simple login system with 3 attempts to enter the correct password.
+int main ()
+{
+    char pw[50], in[50];
+    printf ("Create a Password without spaces and under 50 characters: ");
+    scanf ("%s", pw);
+    for (int i=1; i<=3; i++)
+    {
+        printf ("\n Attempt %d, Enter Password: ", i);
+        scanf ("%s", in);
+        if (strcmp(in, pw) ==0)
+        {
+            printf("\n Password is correct.");
+            break;
+        }
+        else 
+        {
+            printf("\n Password is incorrect, %d tries left ", 3-i);
+        }
+    }
+}
